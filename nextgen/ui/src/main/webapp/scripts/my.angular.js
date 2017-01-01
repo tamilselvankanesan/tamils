4c416bd6a8b4df15182c4c8779d6555efadd3ff0
Tamilselvan <tamil.ts@gmail.com>
2017-01-01 16:24:14 -0500

    angular

--- nextgen/ui/src/main/webapp/scripts/my.angular.js
+++ nextgen/ui/src/main/webapp/scripts/my.angular.js
@@ -7,3 +7,13 @@ myangular.controller("myctrl", function($scope) {
 	$scope.name = "John";
 });
 
+myangular.controller("dd", function($scope) {
+	$scope.ddOptions = ["One", "Two", "Three"];
+	$scope.ddOptionsArr=[
+	                     {val:"1", label:"One"},
+	                     {val:"2", label:"Two"},
+	                     {val:"3", label:"Three"}
+	                     ];
+});
+
+
