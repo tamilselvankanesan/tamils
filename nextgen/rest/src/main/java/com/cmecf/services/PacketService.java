159ba948dcea0fff3f44f901d7bfbb45d21df08f
Tamilselvan <tamil.ts@gmail.com>
2017-01-01 22:49:54 -0500

    angular rest

--- nextgen/rest/src/main/java/com/cmecf/services/PacketService.java
+++ nextgen/rest/src/main/java/com/cmecf/services/PacketService.java
@@ -3,8 +3,11 @@ package com.cmecf.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cmecf.daos.PacketSetDTO;
@@ -19,11 +22,18 @@ public class PacketService {
 	private PacketFacade facade;
 	
 	@RequestMapping(path="/getPacketsSet", method=RequestMethod.GET, produces={"application/json"})
-	public PacketSetDTO getPacketsSet(){
+	public PacketSetDTO getPacketsSet(@RequestParam(name="input") String input){
+		System.out.println("Entered search param :"+input);
 		return facade.getPackets();
 	}
 	@RequestMapping(path="/getPackets", method=RequestMethod.GET, produces={"application/json"})
 	public List<Packet> getPackets(){
+		return facade.getPackets().getPackets();
+	}
+	@RequestMapping(path="/getPacketsByJson", method=RequestMethod.POST, produces={"application/json"}, consumes={"application/json"})
+	public @ResponseBody List<Packet> getPackets(@RequestBody Packet packet){
+		System.out.println("Search using Packet Object");
+		System.out.println(packet.getPacketName());
 		return facade.getPackets().getPackets();
 	}
 }
--- nextgen/ui/src/main/webapp/scripts/my.angular.js
+++ nextgen/ui/src/main/webapp/scripts/my.angular.js
@@ -16,4 +16,35 @@ myangular.controller("dd", function($scope) {
 	                     ];
 });
 
+myangular.controller("packet", function($scope, $http) {
+	//with param
+	$scope.getPackets = function() {
+		$http({method: "GET", 
+			url: "http://localhost:8080/rest/packet/getPacketsSet",
+			params:{"input" : $scope.caseNum}}).then (function(response) {
+				$scope.packetDTO = response.data.packets;		
+			});
+		/*alert($scope.schParam);  //without param
+		$http.get("http://localhost:8080/rest/packet/getPacketsSet").then(function(response) {
+			$scope.packetDTO = response.data.packets;		
+		});*/
+	}
+	$scope.getPacketsUsingJson = function() {
+		var jsonParamObj = {packetName: $scope.pName, doc: $scope.docNum};
+		var res = $http.post("http://localhost:8080/rest/packet/getPacketsByJson", jsonParamObj);
+		/*$http.post("http://localhost:8080/rest/packet/getPacketsByJson", JSON.stringify(jsonParamObj)).success(function(response) {
+			alert('ss');
+		}).error(function(response) {
+			alert('s1s');
+		});*/ //doesn't work . post.success doesn't work
+		
+		res.then(function(data) {
+			$scope.packetDTO = data.data;
+		});//works
+		
+		/*$http.get("http://localhost:8080/rest/packet/getPacketsByJson", jsonParamObj).then(function(response) {
+			$scope.packetDTO = response.data.packets;
+		});*/ //cannot send JSON body in GET request
+	}
+});
 
