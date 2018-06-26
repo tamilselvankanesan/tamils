6b9003eeaf1c2699c122f9ad5e4676001f8cf439
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2018-06-26 15:28:09 -0400

    json2xsd test

---
+++ Test/src/com/success/utils/Json2Xsd.java
@@ -0,0 +1,28 @@
+package com.success.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.w3c.dom.Document;
+
+import com.ethlo.jsons2xsd.Config;
+import com.ethlo.jsons2xsd.Jsons2Xsd;
+import com.ethlo.jsons2xsd.XmlUtil;
+
+public class Json2Xsd {
+
+	public static void main(String[] args) {
+		try {
+			Reader r = new InputStreamReader(new FileInputStream(new File("C:\\Users\\btamilselvan\\Desktop\\a.json")));
+			final Config cfg = new Config.Builder().targetNamespace("http://example.com/myschema.xsd").name("array").build();
+			final Document doc = Jsons2Xsd.convert(r, cfg);
+			System.out.println(XmlUtil.asXmlString(doc.getDocumentElement()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
