6b4cfcf0469210fea2ddaef44a7d712995b95b8d
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2018-07-10 16:43:57 -0400

    Password validation related changes

--- moi/src/app/home/password_validator.ts
+++ moi/src/app/home/password_validator.ts
@@ -1,4 +1,4 @@
-import {Directive, forwardRef} from '@angular/core';
+import {Directive, forwardRef, Attribute} from '@angular/core';
 import {NG_VALIDATORS, AbstractControl} from '@angular/forms';
 
 @Directive({
@@ -17,21 +17,64 @@ export class PasswordValidator {
     return hasNumber && hasUpper && hasLower;
   }
 
+  constructor( @Attribute('passwordValidator') public targetComponent: string, @Attribute('reverse') public reverse: string) {}
+
   validate(c: AbstractControl): {[key: string]: boolean | null} {
-    if (c.parent.get('confirmPasswd') && c.parent.get('passwd') && c.parent.get('confirmPasswd').value && c.parent.get('passwd').value) {
-      let cp = c.parent.get('confirmPasswd').value;
-      let p = c.parent.get('passwd').value;
-      if (cp != p) {
-        c.parent.get('confirmPasswd').setErrors({Match: true});
-        console.log('doesnt match = :' + cp + ' :p: ' + p);
-        return {'Match': false};
+
+    if (c && c.parent.get(this.targetComponent)) {
+      console.log('clear = ');
+      c.setErrors({Match: true});
+      c.parent.get(this.targetComponent).setErrors({Match: true});
+      
+      
+      c.setErrors(null);
+      c.parent.get(this.targetComponent).setErrors(null);
+
+      if (c.value && c.parent.get(this.targetComponent).value) {
+
+        console.log('inside ' + c.value);
+        console.log('inside target ' + c.root.get(this.targetComponent).value);
+        if (c.value !== c.parent.get(this.targetComponent).value) {
+          console.log('doesnt match = :');
+
+          c.setErrors({'Match': false});
+          c.parent.get(this.targetComponent).setErrors(null);
+
+          //        if (this.reverse) {
+          //          console.log(' password comp = :');
+          //          c.parent.get(this.targetComponent).setErrors(null);
+          //          c.setErrors({Match: false});
+          //        } else {
+          //          console.log(' confirm password comp = :');
+          //          c.parent.get(this.targetComponent).setErrors(null);
+          //          c.parent.get('confirmPasswd').setErrors({Match: false});
+          //        }
+          return {'Match': false};
+        }
+        console.log(' password match ');
+        c.setErrors(null);
+        c.parent.get(this.targetComponent).setErrors(null);
+
       }
-      if (!PasswordValidator.strong(c)) {
-        c.parent.get('confirmPasswd').setErrors({Strong: true});
-        console.log('not a strong password = ');
-        return {'Strong': false};
-      }
+
+
     }
+
+
+    //    if (c.parent.get('confirmPasswd') && c.parent.get('passwd') && c.parent.get('confirmPasswd').value && c.parent.get('passwd').value) {
+    //      let cp = c.parent.get('confirmPasswd').value;
+    //      let p = c.parent.get('passwd').value;
+    //      if (cp != p) {
+    //        c.parent.get('confirmPasswd').setErrors({Match: true});
+    //        console.log('doesnt match = :' + cp + ' :p: ' + p);
+    //        return {'Match': false};
+    //      }
+    //      if (!PasswordValidator.strong(c)) {
+    //        c.parent.get('confirmPasswd').setErrors({Strong: true});
+    //        console.log('not a strong password = ');
+    //        return {'Strong': false};
+    //      }
+    //    }
     return null;
   }
 }
