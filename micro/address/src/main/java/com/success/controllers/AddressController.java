df7d39d0715ccf0d2ec0678f389f9a475bd6ec88
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2019-03-25 17:09:01 -0400

    load config from git repo

--- micro/address/src/main/java/com/success/controllers/AddressController.java
+++ micro/address/src/main/java/com/success/controllers/AddressController.java
@@ -2,15 +2,22 @@ package com.success.controllers;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RefreshScope
 public class AddressController {
 
+	@Value("${message: default hello}")
+	private String message;
+	
 	@GetMapping(path="/{personId}")
 	public String getAddress(@PathVariable("personId") String personId){
+		System.out.println("message from properties file "+message);
 		return "from Address controller. Given person ID is "+personId+". Current date is "+new Date()+" My instance ID is "+this.toString();
 	}
 	
