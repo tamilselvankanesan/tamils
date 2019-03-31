640535a5b9179b554386337c682f0f337fdf98e6
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2019-03-31 11:49:13 -0400

    Default proxy path added

--- micro/proxy/src/main/java/com/success/ProxyApplication.java
+++ micro/proxy/src/main/java/com/success/ProxyApplication.java
@@ -2,12 +2,13 @@ package com.success;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-public class ProxyApplication {
+public class ProxyApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
