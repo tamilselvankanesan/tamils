2dd66b8c63b6fe838bfce81ae63eda0da7cdd163
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2019-04-04 15:49:15 -0400

    util module added

--- micro/address/src/main/java/com/success/AddressApplicaiton.java
+++ micro/address/src/main/java/com/success/AddressApplicaiton.java
@@ -3,13 +3,13 @@ package com.success;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.commons.util.InetUtils;
+//import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
+//import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Profile;
 
-import com.netflix.appinfo.AmazonInfo;
+//import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -19,20 +19,22 @@ public class AddressApplicaiton extends SpringBootServletInitializer {
 		SpringApplication.run(AddressApplicaiton.class, args);
 	}
 
-	@Bean
-	@Profile("aws")
-	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
-		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
-
-		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-		config.setDataCenterInfo(info);
-		info.getMetadata().put(AmazonInfo.MetaDataKey.publicHostname.getName(),
-				info.get(AmazonInfo.MetaDataKey.publicIpv4));
-		config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
-		config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
-//		config.setNonSecurePort(port);
-		config.setDataCenterInfo(info);
-		return config;
-	}
+	/*
+	 * for ec2 - not applicable for fargate type
+	 * @Bean
+	 * 
+	 * @Profile("aws") public EurekaInstanceConfigBean
+	 * eurekaInstanceConfig(InetUtils inetUtils) { EurekaInstanceConfigBean config =
+	 * new EurekaInstanceConfigBean(inetUtils);
+	 * 
+	 * AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+	 * config.setDataCenterInfo(info);
+	 * info.getMetadata().put(AmazonInfo.MetaDataKey.publicHostname.getName(),
+	 * info.get(AmazonInfo.MetaDataKey.publicIpv4));
+	 * config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+	 * config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4)); //
+	 * config.setNonSecurePort(port); config.setDataCenterInfo(info); return config;
+	 * }
+	 */
 
 }
---
+++ micro/address/src/main/java/com/success/CloudConfig.java
@@ -0,0 +1,92 @@
+package com.success;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import com.netflix.appinfo.AmazonInfo;
+import com.success.util.aws.Constants;
+import com.success.util.aws.Container;
+import com.success.util.aws.Converter;
+import com.success.util.aws.EcsTaskMetadata;
+import com.success.util.aws.Network;
+
+@Configuration
+public class CloudConfig {
+
+	private final Environment env;
+	// Used as string.contains to search correct container
+    // Make sure that your Docker container in AWS Task definition has this as part of its name
+    @Value("${aws.containerName}")
+    private String containerName;
+	
+	public CloudConfig(Environment env){
+        this.env = env;
+    }
+
+	@Bean
+	@Profile("aws")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		
+		System.out.println("container name is "+containerName);
+
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		config.setDataCenterInfo(info);
+
+		try {
+			String json = readEcsMetadata();
+			EcsTaskMetadata metadata = Converter.fromJsonString(json);
+			String ipAddress = findContainerPrivateIP(metadata);
+			config.setIpAddress(ipAddress);
+			config.setNonSecurePort(getPortNumber());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return config;
+	}
+
+	private int getPortNumber() {
+		return env.getProperty("server.port", Integer.class);
+	}
+
+	private String findContainerPrivateIP(EcsTaskMetadata metadata) {
+		if (null != metadata) {
+			for (Container container : metadata.getContainers()) {
+				boolean found = container.getName().toLowerCase().contains(containerName);
+				if (found) {
+					Network network = container.getNetworks()[0];
+					return network.getIPv4Addresses()[0];
+				}
+			}
+		}
+		return "";
+	}
+	private String readEcsMetadata() throws Exception {
+		String url = Constants.AWS_METADATA_URL;
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		StringBuilder response = new StringBuilder();
+		try {
+			con.setRequestMethod("GET");
+			try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+				String inputLine;
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+			}
+		} finally {
+			con.disconnect();
+		}
+		return response.toString();
+	}
+}
