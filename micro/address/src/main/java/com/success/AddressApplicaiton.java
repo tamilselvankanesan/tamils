a9623da58123e8ac80e45e40a811f92f5ba5ac2d
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2019-04-06 19:55:48 -0400

    using ec2

--- micro/address/src/main/java/com/success/AddressApplicaiton.java
+++ micro/address/src/main/java/com/success/AddressApplicaiton.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 //import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Profile;
-
-//import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,23 +13,5 @@ public class AddressApplicaiton extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(AddressApplicaiton.class, args);
 	}
-
-	/*
-	 * for ec2 - not applicable for fargate type
-	 * @Bean
-	 * 
-	 * @Profile("aws") public EurekaInstanceConfigBean
-	 * eurekaInstanceConfig(InetUtils inetUtils) { EurekaInstanceConfigBean config =
-	 * new EurekaInstanceConfigBean(inetUtils);
-	 * 
-	 * AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-	 * config.setDataCenterInfo(info);
-	 * info.getMetadata().put(AmazonInfo.MetaDataKey.publicHostname.getName(),
-	 * info.get(AmazonInfo.MetaDataKey.publicIpv4));
-	 * config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
-	 * config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4)); //
-	 * config.setNonSecurePort(port); config.setDataCenterInfo(info); return config;
-	 * }
-	 */
 
 }
--- micro/address/src/main/java/com/success/CloudConfig.java
+++ micro/address/src/main/java/com/success/CloudConfig.java
@@ -22,41 +22,61 @@ import org.springframework.core.env.Environment;
 
 import com.netflix.appinfo.AmazonInfo;
 import com.success.util.aws.Constants;
-import com.success.util.aws.Container;
-import com.success.util.aws.EcsTaskMetadata;
-import com.success.util.aws.Network;
 
 @Configuration
 public class CloudConfig {
 
 	private final Environment env;
 	// Used as string.contains to search correct container
-    // Make sure that your Docker container in AWS Task definition has this as part of its name
-    @Value("${aws.containerName}")
-    private String containerName;
-	
-	public CloudConfig(Environment env){
-        this.env = env;
-    }
+	// Make sure that your Docker container in AWS Task definition has this as part
+	// of its name
+	@Value("${aws.containerName}")
+	private String containerName;
+
+	public CloudConfig(Environment env) {
+		this.env = env;
+	}
+
+	// for ec2 - not applicable for fargate type
 
 	@Bean
+
 	@Profile("aws")
 	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
-		
-		System.out.println("container name is "+containerName);
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		System.out.println("amazon info ->" + info.toString());
+		config.setDataCenterInfo(info);
+		info.getMetadata().put(AmazonInfo.MetaDataKey.publicHostname.getName(),
+				info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+		config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4)); //
+		config.setNonSecurePort(getPortNumber());
+		config.setDataCenterInfo(info);
+		return config;
+	}
+
+//	@Bean
+	@Profile("aws1")
+	public EurekaInstanceConfigBean eurekaInstanceConfig1(InetUtils inetUtils) {
+
+		System.out.println("container name is " + containerName);
 
 		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
 		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-		System.out.println("amazon info ->"+info.toString());
+//		AmazonInfo.MetaDataKey.localIpv4;
+		System.out.println("amazon info ->" + info.toString());
 		config.setDataCenterInfo(info);
 
 		try {
 			String json = readEcsMetadata();
-			System.out.println("ECS meta-data is "+json);
+			System.out.println("ECS meta-data is " + json);
 //			EcsTaskMetadata metadata = Converter.fromJsonString(json);
 //			String ipAddress = findContainerPrivateIP(metadata);
 			config.setIpAddress(getPrivateIpV4Address(json));
-			config.setNonSecurePort(getPortNumber());
+			config.setNonSecurePort(8080);
+			getPortNumber();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -64,21 +84,17 @@ public class CloudConfig {
 	}
 
 	private int getPortNumber() {
+		System.out.println("port number is " + env.getProperty("server.port", Integer.class));
 		return env.getProperty("server.port", Integer.class);
 	}
 
-	private String findContainerPrivateIP(EcsTaskMetadata metadata) {
-		if (null != metadata) {
-			for (Container container : metadata.getContainers()) {
-				boolean found = container.getName().toLowerCase().contains(containerName);
-				if (found) {
-					Network network = container.getNetworks()[0];
-					return network.getIPv4Addresses()[0];
-				}
-			}
-		}
-		return "";
-	}
+	/*
+	 * private String findContainerPrivateIP(EcsTaskMetadata metadata) { if (null !=
+	 * metadata) { for (Container container : metadata.getContainers()) { boolean
+	 * found = container.getName().toLowerCase().contains(containerName); if (found)
+	 * { Network network = container.getNetworks()[0]; return
+	 * network.getIPv4Addresses()[0]; } } } return ""; }
+	 */
 	private String readEcsMetadata() throws Exception {
 		String url = Constants.AWS_METADATA_URL;
 		URL obj = new URL(url);
@@ -97,57 +113,53 @@ public class CloudConfig {
 		}
 		return response.toString();
 	}
-	
+
 	private String getPrivateIpV4Address(String data) {
 		try {
 			JSONParser jsonParser = new JSONParser();
-			JSONObject obj = (JSONObject)jsonParser.parse(data);
-            JSONArray containers = (JSONArray)obj.get("Containers");
-            for(Object containerObj : containers) {
-            	JSONObject container = (JSONObject)containerObj;
-            	if(!container.get("Name").toString().equals("address-service")) {
-            		continue;
-            	}
-            	 JSONArray networks = (JSONArray)container.get("Networks");
-            	 JSONObject nw = (JSONObject)networks.get(0);
-            	 JSONArray ipv4Addresses = (JSONArray)nw.get("IPv4Addresses");
-            	 System.out.println("ip address is "+ipv4Addresses.get(0));
-            	 return (String)ipv4Addresses.get(0);
-            }
+			JSONObject obj = (JSONObject) jsonParser.parse(data);
+			JSONArray containers = (JSONArray) obj.get("Containers");
+			for (Object containerObj : containers) {
+				JSONObject container = (JSONObject) containerObj;
+				if (!container.get("Name").toString().equals("address-service")) {
+					continue;
+				}
+				JSONArray networks = (JSONArray) container.get("Networks");
+				JSONObject nw = (JSONObject) networks.get(0);
+				JSONArray ipv4Addresses = (JSONArray) nw.get("IPv4Addresses");
+				System.out.println("ip address is " + ipv4Addresses.get(0));
+				return (String) ipv4Addresses.get(0);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
+
 	public static void main(String[] args) {
 		JSONParser jsonParser = new JSONParser();
-		try (FileReader reader = new FileReader("C:\\Users\\btamilselvan\\Google Drive\\Documents\\study_material\\AWS\\aws_meta.json"))
-        {
-            //Read JSON file
-            JSONObject obj = (JSONObject)jsonParser.parse(reader);
-            JSONArray containers = (JSONArray)obj.get("Containers");
-            for(Object containerObj : containers) {
-            	JSONObject container = (JSONObject)containerObj;
-            	if(!container.get("Name").toString().equals("address-service")) {
-            		continue;
-            	}
-            	 JSONArray networks = (JSONArray)container.get("Networks");
-            	 JSONObject nw = (JSONObject)networks.get(0);
-            	 JSONArray ipv4Addresses = (JSONArray)nw.get("IPv4Addresses");
-            	 System.out.println("dasdasdsa "+ipv4Addresses.get(0)); 	 
-            }
-            System.out.println("dasdasdsa");
- 
-//            JSONArray employeeList = (JSONArray) obj;
- 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-		
+		try (FileReader reader = new FileReader("")) {
+			// Read JSON file
+			JSONObject obj = (JSONObject) jsonParser.parse(reader);
+			JSONArray containers = (JSONArray) obj.get("Containers");
+			for (Object containerObj : containers) {
+				JSONObject container = (JSONObject) containerObj;
+				if (!container.get("Name").toString().equals("address-service")) {
+					continue;
+				}
+				JSONArray networks = (JSONArray) container.get("Networks");
+				JSONObject nw = (JSONObject) networks.get(0);
+				JSONArray ipv4Addresses = (JSONArray) nw.get("IPv4Addresses");
+				System.out.println("dasdasdsa " + ipv4Addresses.get(0));
+			}
+			System.out.println("dasdasdsa");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
