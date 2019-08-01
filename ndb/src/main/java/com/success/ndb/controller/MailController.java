f2b5a03cfddb551d74ba1259b3c0f73a52937b02
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2019-08-01 15:36:08 -0400

    Print all params

--- ndb/src/main/java/com/success/ndb/controller/MailController.java
+++ ndb/src/main/java/com/success/ndb/controller/MailController.java
@@ -2,7 +2,10 @@ package com.success.ndb.controller;
 
 import java.util.Map;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,20 +15,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class MailController {
 
 	@PostMapping
-	public String receive(@RequestParam Map<String,String> allParams) {
+	public Result receive(@RequestParam Map<String,String> allParams, @RequestHeader Map<String,String> allHeaders) {
+		Result r = new Result();
+		r.allParams = allParams;
+		r.allHeaders = allHeaders;
 		
-		StringBuilder mail = new StringBuilder();
-		allParams.forEach((key, value) -> {
-			
-			mail.append("Key: ");
-			mail.append(key);
-			mail.append(" value: ");
-			mail.append(value);
-			mail.append("\n");
-		});
-		
-		System.out.println("mail content is "+mail.toString());
-		
-		return mail.toString();
+		  StringBuilder mail = new StringBuilder(); allParams.forEach((key, value) -> {
+		  
+		  mail.append("Key: "); mail.append(key); mail.append("\n");
+		  mail.append(" value: "); mail.append(value); mail.append("\n"); });
+		  
+		  System.out.println("mail content is "+mail.toString());
+		 
+		return r;
+	}
+	
+	@XmlRootElement
+	static class Result{
+		public Map<String, String> getAllParams() {
+			return allParams;
+		}
+		public void setAllParams(Map<String, String> allParams) {
+			this.allParams = allParams;
+		}
+		public Map<String, String> getAllHeaders() {
+			return allHeaders;
+		}
+		public void setAllHeaders(Map<String, String> allHeaders) {
+			this.allHeaders = allHeaders;
+		}
+		Map<String,String> allParams;
+		Map<String,String> allHeaders;
 	}
 }
