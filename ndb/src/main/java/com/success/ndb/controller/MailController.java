11cc9c33154b2ce4cc60cd9e3593f86effa5e565
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2019-08-01 12:59:04 -0400

    Add mailcontroller

--- ndb/src/main/java/com/success/ndb/controller/MailController.java
+++ ndb/src/main/java/com/success/ndb/controller/MailController.java
@@ -2,14 +2,17 @@ package com.success.ndb.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/mail")
+@RequestMapping(path="/rest/mail")
 public class MailController {
 
+	private static final Logger logger = Logger.getLogger(MailController.class);
+	
 	@PostMapping
 	public String receive(HttpServletRequest request) {
 		StringBuilder mail = new StringBuilder();
@@ -18,6 +21,8 @@ public class MailController {
 		
 		mail.append(" Body: ");
 		mail.append(request.getHeader("HTML"));
+		
+		logger.info("mail content is "+mail.toString());
 		
 		return mail.toString();
 	}
