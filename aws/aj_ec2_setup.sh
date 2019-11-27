51603e4cdbfa1f8fa3f45614ffedd8b962928707
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2019-11-27 15:28:18 -0500

    aj ec2 setup script added

---
+++ aws/aj_ec2_setup.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+yum update -y
+#should be removed - for centos
+#yum install ncurses initscripts -y
+
+yum install java-1.8.0-openjdk wget unzip nginx python3 -y
+
+#install nginx
+#only for aws linux 2
+amazon-linux-extras install nginx1.12
+
+#yum install nginx
+
+#install aws cli
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+export PATH=~/.local/bin:$PATH
+source ~/.bash_profile
+pip3 install awscli --upgrade --user
+
+################ download artifacts from s3 start #####################################
+mkdir /opt/appdata
+cd /opt/appdata
+#get log4j.properties
+aws s3api get-object --bucket=ndb-artifacts --key=log4j.properties log4j.properties
+
+#get war file
+aws s3api get-object --bucket=ndb-artifacts --key=services.war services.war
+
+#get UI code
+aws s3api get-object --bucket=ndb-artifacts --key=AppliedJobsWeb.zip AppliedJobsWeb.zip
+
+#get appjob webservice configuration
+aws s3api get-object --bucket=ndb-artifacts --key=applied_jobs_config.sh applied_jobs_config.sh
+
+#get APPJOBS_TOMCAT
+aws s3api get-object --bucket=ndb-artifacts --key=APPJOBS_TOMCAT APPJOBS_TOMCAT
+
+#get nginx.conf
+aws s3api get-object --bucket=ndb-artifacts --key=nginx.conf nginx.conf
+
+################ download from s3 end #####################################
+
+################ configure tomcat start #####################################
+##download tomcat
+wget http://mirror.olnevhost.net/pub/apache/tomcat/tomcat-9/v9.0.27/bin/apache-tomcat-9.0.27.tar.gz
+
+##extract tomcat to /opt/tomcat9
+mkdir -p /opt/tomcat9
+tar -zxvf apache-tomcat-9.0.27.tar.gz -C /opt/tomcat9 --strip-components=1
+
+#add tomcat group
+groupadd tomcat
+
+#create tomcat user
+useradd -s /bin/nologin -g tomcat -d /opt/tomcat9 tomcat
+
+cd /opt
+ln -s /opt/tomcat9 /opt/tomcat
+
+cp /opt/appdata/log4j.properties /opt/tomcat/conf
+chown tomcat:root /opt/tomcat/conf/log4j.properties
+
+cp /opt/appdata/services.war /opt/tomcat/webapps
+chown tomcat:root /opt/tomcat/webapps/services.war
+
+#copy appjob webservice configuration
+mkdir /opt/data
+cp /opt/appdata/applied_jobs_config.sh /opt/data
+chmod 755 /opt/data/applied_jobs_config.sh
+chown tomcat:root /opt/data/applied_jobs_config.sh
+
+chmod 775 /opt/tomcat9
+chmod 775 /opt/tomcat9/webapps/
+chown -R tomcat:root /opt/tomcat9
+
+mkdir -p /var/log/tomcat
+chown tomcat:root /var/log/tomcat
+chmod 755 /var/log/tomcat
+
+#tomcat start/stop script.. as a service
+cp /opt/appdata/APPJOBS_TOMCAT /etc/init.d
+cd /etc/init.d
+chmod 775 APPJOBS_TOMCAT
+chown -R tomcat:root APPJOBS_TOMCAT
+chkconfig --add APPJOBS_TOMCAT
+chkconfig APPJOBS_TOMCAT on
+
+################ configure tomcat end #####################################
+
+################ configure nginx start #####################################
+
+#copy nginx.conf
+cp /opt/appdata/nginx.conf /etc/nginx/nginx.conf
+chmod 644 /etc/nginx/nginx.conf
+cd /opt/appdata
+unzip AppliedJobsWeb.zip -d AppliedJobsWeb
+rm -rf /var/www/html/AppliedJobsWeb
+mkdir -p /var/www/html/AppliedJobsWeb
+cp -r /opt/appdata/AppliedJobsWeb/build/* /var/www/html/AppliedJobsWeb
+chmod  744 /var/www/html/AppliedJobsWeb
+#enable nginx service
+systemctl enable nginx
+
+################ configure nginx end #####################################
+
+#start nginx and tomcat
+service nginx start
+service APPJOBS_TOMCAT start
+
+###################################################################
--- aws1/awssimple/src/main/java/com/success/s3/SimpleS3.java
+++ aws1/awssimple/src/main/java/com/success/s3/SimpleS3.java
@@ -1,39 +1,78 @@
 package com.success.s3;
 
-import java.util.concurrent.ExecutionException;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.sts.StsAsyncClient;
-import software.amazon.awssdk.services.sts.StsClient;
-import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.amazonaws.HttpMethod;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest.KeyVersion;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class SimpleS3 {
 
-  public static void main(String[] args) throws InterruptedException, ExecutionException {
-    
-    //make sure credentials and config files are there under users/{username}/.aws directory.. with access key and security key and profile information
-//    S3AsyncClient s3Client = S3AsyncClient.builder().region(Region.US_EAST_1).credentialsProvider(ProfileCredentialsProvider.builder().profileName("selvan").build()).build();
-    S3AsyncClient s3Client = S3AsyncClient.builder().region(Region.US_EAST_1).build(); // works ..uses the default profile
-    System.out.println(s3Client.toString());
-    System.out.println(s3Client.listBuckets().get().buckets().size());
-    s3Client.listBuckets().get().buckets().forEach(e -> System.out.println(e.name()));
-    
-    StsClient sClient = StsClient.builder().region(Region.US_EAST_1).credentialsProvider(ProfileCredentialsProvider.builder().profileName("selvan").build()).build();
-    
-    StsAsyncClient stsClient = StsAsyncClient.builder().region(Region.US_EAST_1).credentialsProvider(ProfileCredentialsProvider.builder().profileName("selvan").build()).build();
-    AssumeRoleRequest assumeRoleRequest = AssumeRoleRequest.builder().roleArn("arn:aws:iam::326286503284:role/some").roleSessionName("some").build();
-//    System.out.println(stsClient.assumeRole(assumeRoleRequest).get().credentials().accessKeyId());
-    System.out.println(sClient.assumeRole(assumeRoleRequest).credentials());
-    
-    
-//    AWSSecurityTokenService tokenService = AWSSecurityTokenServiceClientBuilder.standard().withCredentials().withRegion(Region.US_EAST_1.toString()).build();
-    
-    //the IAM user principal should be in the trust relationship, also the role should have a assumeRole permission policy
-    /*AssumeRoleRequest request = new AssumeRoleRequest().withRoleArn("arn:aws:iam::326286503284:role/some").withRoleSessionName("tamil")
-        .withDurationSeconds(3600);
-    AssumeRoleResult result = tokenService.assumeRole(request);
-    System.out.println(result.getCredentials().getAccessKeyId());*/
-  }
+//  public static void main(String[] args) throws InterruptedException, ExecutionException {
+//    
+//    //make sure credentials and config files are there under users/{username}/.aws directory.. with access key and security key and profile information
+////    S3AsyncClient s3Client = S3AsyncClient.builder().region(Region.US_EAST_1).credentialsProvider(ProfileCredentialsProvider.builder().profileName("selvan").build()).build();
+//    S3AsyncClient s3Client = S3AsyncClient.builder().region(Region.US_EAST_1).build(); // works ..uses the default profile
+//    System.out.println(s3Client.toString());
+//    System.out.println(s3Client.listBuckets().get().buckets().size());
+//    s3Client.listBuckets().get().buckets().forEach(e -> System.out.println(e.name()));
+//    
+//    StsClient sClient = StsClient.builder().region(Region.US_EAST_1).credentialsProvider(ProfileCredentialsProvider.builder().profileName("selvan").build()).build();
+//    
+//    StsAsyncClient stsClient = StsAsyncClient.builder().region(Region.US_EAST_1).credentialsProvider(ProfileCredentialsProvider.builder().profileName("selvan").build()).build();
+//    AssumeRoleRequest assumeRoleRequest = AssumeRoleRequest.builder().roleArn("arn:aws:iam::326286503284:role/some").roleSessionName("some").build();
+////    System.out.println(stsClient.assumeRole(assumeRoleRequest).get().credentials().accessKeyId());
+//    System.out.println(sClient.assumeRole(assumeRoleRequest).credentials());
+//    
+//    
+////    AWSSecurityTokenService tokenService = AWSSecurityTokenServiceClientBuilder.standard().withCredentials().withRegion(Region.US_EAST_1.toString()).build();
+//    
+//    //the IAM user principal should be in the trust relationship, also the role should have a assumeRole permission policy
+//    /*AssumeRoleRequest request = new AssumeRoleRequest().withRoleArn("arn:aws:iam::326286503284:role/some").withRoleSessionName("tamil")
+//        .withDurationSeconds(3600);
+//    AssumeRoleResult result = tokenService.assumeRole(request);
+//    System.out.println(result.getCredentials().getAccessKeyId());*/
+//  }
+
+	public static void main(String[] args) {
+		System.out.println("testing");
+		List<KeyVersion> keyList = new ArrayList<KeyVersion>();
+
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1)
+				.withCredentials(new ProfileCredentialsProvider("app_user")).build();
+		ListObjectsV2Request req = new ListObjectsV2Request().withBucketName("applied-jobs")
+				.withPrefix("dev/resumes/a6867334-2e61-46d6-8665-6a485511bb66");
+		ListObjectsV2Result result = s3Client.listObjectsV2(req);
+		for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
+			System.out.printf(" - %s (size: %d)\n", objectSummary.getKey(), objectSummary.getSize());
+			keyList.add(new KeyVersion(objectSummary.getKey()));
+		}
+		keyList.add(new KeyVersion("927b996f-89c0-4f13-9ba2-3e729a231a32"));
+
+		Date expiration = new Date();
+		long expTimeMillis = expiration.getTime();
+		expTimeMillis += 60000;
+		expiration.setTime(expTimeMillis);
+
+		GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest("applied-jobs",
+				"dev/resumes/a6867334-2e61-46d6-8665-6a485511bb66/a6867334-2e61-46d6-8665-6a485511bb66.pdf").withMethod(HttpMethod.GET)
+						.withExpiration(expiration);
+		URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+		System.out.println(url.toString());
+
+//         s3Client.deleteObjects(new DeleteObjectsRequest("applied-jobs1").withKeys(keyList));
+
+	}
 
 }
