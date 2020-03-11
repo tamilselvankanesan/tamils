8a3692519cb397bbefe172e7487344f3cd912a31
Tamil <tamilselvan_balasubramaniam@ao.uscourts.gov>
2020-03-11 19:05:48 -0400

    more python workouts

--- hellopy/com/success/first.py
+++ hellopy/com/success/first.py
@@ -3,48 +3,147 @@ Created on Mar 10, 2020
 
 @author: btamilselvan
 '''
-import string
-print('''hello world testing ''')
+import re
+from re import match
 
-# print('add two number: ')
-# num1 = int(input('enter number 1: '))
-# num2 = int(input('enter number 2: '))
-# print('sum is %d' %(num1+num2))
+def hw():
+    print('''hello world testing ''')
 
-print('\n Find the Square Root: ')
 
-for i in range(17):
-    print('Square root of %d is %d' %(i, i**.5))
+def add():
+    print('add two number: ')
+    num1 = int(input('enter number 1: '))
+    num2 = int(input('enter number 2: '))
+    print('sum is %d' %(num1+num2))
+
+def sr():
+    print('\n Find the Square Root: ')
+    for i in range(17):
+        print('Square root of %d is %d' %(i, i**.5))
+
+def swap():
+    print("swap two numbers.. \n")
+    num1 = int(input("enter number 1:"))
+    num2 = int(input("enter number 2:"))
+    num1,num2 = num2,num1
+    print("after swapping num1 is {}, num2 is {}".format(num1, num2))
     
-# print("swap two numbers.. \n")
-# num1 = int(input("enter number 1:"))
-# num2 = int(input("enter number 2:"))
-# num1,num2 = num2,num1
-# print("after swapping num1 is {}, num2 is {}".format(num1, num2))
+def listfun():
+    
+    mList = [0, 1, 2,4]
+    print(mList.__dir__())
+    print('original list', mList)
+    mList.reverse()
+    print('reversed list', *mList)
+    
+    mList = [range(1, 50, 10)]
+    print(mList)
+    
+    mList = [i*2 for i in range(10)]
+    print(mList)
+    
+    for i in range(10):
+        mList.append(i)
+    print(mList)
 
-mList = [0, 1, 2,4]
-print(mList.__dir__())
-print('original list', mList)
-mList.reverse()
-print('reversed list', *mList)
+def strfun():
+    mystr = ["{:03d}".format(i) for i in range(10)]
+    print(mystr)
+    resStr = '';
+    for i in mystr:
+        resStr +=i
+    print(resStr)
+    resStr.join(mystr)
+    print(resStr)
 
-mList = [range(1, 50, 10)]
-print(mList)
+def countAllUrl(aName):
+    result_set=set()
+#     print('before start')
+    tFile = open(aName, 'r')
+    count = 0
+    pattern='(GET|POST|PUT|DELETE) (.+) HTTP'
+    sPattern='(.+)\?'
+    for line in tFile:
+        match = re.search(pattern, line)
+        if match:
+            mystr = match.group(2)
+            index = mystr.find('server.php')
+            if index>0:
+                smatch = re.search(sPattern, mystr)
+                if smatch:
+                    result_set.add(smatch.group(1))
+            else:
+                result_set.add(match.group(2))
+                count +=1
+#         if count==1000:
+#             break
+#         print(line)
+#     tFile.read()
+    tFile.close()
+#     print(result_set)
+    for i in result_set:
+#         print(i)
+        pass
+    return count
 
-mList = [i*2 for i in range(10)]
-print(mList)
+def countUrl(url, aName):
+#     print('before start')
+    tFile = open(aName, 'r')
+    count = 0
+    pattern='(GET|POST|PUT|DELETE) (.+) HTTP'
+    for line in tFile:
+        
+        match = re.search(pattern, line)
+        if match:
+            mystr = match.group(2)
+            index = mystr.find(url)
+            if index>0:
+                count +=1
+    tFile.close()
+    return count
+        
+def unimplemented():
+    pass
 
-for i in range(10):
-    mList.append(i)
-print(mList)
+def extract(aName):
+#     aFile = open(file='C:\\Tamil\\temp\\09Mar15And16LiveAccess.log', mode='w')
+#     aFile.write('Hello world')
+#     aFile.close()
+    with open(file=aName, mode='w') as bFile:
+        aFile = open('C:\\Tamil\\temp\\09MarLiveAccess.log', mode='r')
+        aPattern = '(2020:15|2020:16)'
+        for aLine in aFile:
+            aMatch = re.search(aPattern, aLine)
+            if aMatch:
+                bFile.write(aLine)   
+        bFile.close()
+        aFile.close()
+    
 
-mystr = 'hello'
-mystr = ["{:03d}".format(i) for i in range(10)]
-print(mystr)
-resStr = '';
-for i in mystr:
-    resStr +=i
-print(resStr)
-resStr.join(mystr)
-print(resStr)
+def extractUnique(aName):
+    unique = set()
+    with open(aName, 'r') as aFile:
+        aPattern = '(GET|POST|DELETE|PUT) \/([\/\w-]+)'
+        for aLine in aFile:
+            aMatch = re.search(aPattern, aLine)
+            if aMatch:
+                unique.add(aMatch.group(2))
+    return unique
+    
+# count = countAllUrl('C:\\Tamil\\temp\\09MarLiveAccess.log')
+# print('others count %d' %count)
+# count = countUrl('server.php', 'C:\\Tamil\\temp\\09MarLiveAccess.log')
+# print('server.php count %d' %count)
+bFile = 'C:\\Tamil\\temp\\09Mar15And16LiveAccess.log'
+extract(bFile)
+count = countUrl('server.php', bFile)
+print('server.php count %d' %count)
+
+count = countAllUrl(bFile)
+print('others count %d' %count)
+
+aUnique = extractUnique(bFile)
+# print(aUnique)
+# for i in aUnique:
+#     print(i)
 
