84902e22b0b2370b9e4d3056897a0dc933831a1f
Tamil <tamil@bnsouth.com>
2020-03-10 20:44:59 -0400

    more python workouts

--- hellopy/com/success/first.py
+++ hellopy/com/success/first.py
@@ -3,6 +3,7 @@ Created on Mar 10, 2020
 
 @author: btamilselvan
 '''
+import string
 print('''hello world testing ''')
 
 # print('add two number: ')
@@ -14,3 +15,36 @@ print('\n Find the Square Root: ')
 
 for i in range(17):
     print('Square root of %d is %d' %(i, i**.5))
+    
+# print("swap two numbers.. \n")
+# num1 = int(input("enter number 1:"))
+# num2 = int(input("enter number 2:"))
+# num1,num2 = num2,num1
+# print("after swapping num1 is {}, num2 is {}".format(num1, num2))
+
+mList = [0, 1, 2,4]
+print(mList.__dir__())
+print('original list', mList)
+mList.reverse()
+print('reversed list', *mList)
+
+mList = [range(1, 50, 10)]
+print(mList)
+
+mList = [i*2 for i in range(10)]
+print(mList)
+
+for i in range(10):
+    mList.append(i)
+print(mList)
+
+mystr = 'hello'
+mystr = ["{:03d}".format(i) for i in range(10)]
+print(mystr)
+resStr = '';
+for i in mystr:
+    resStr +=i
+print(resStr)
+resStr.join(mystr)
+print(resStr)
+
