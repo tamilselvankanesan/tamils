dcef2badf89f672c5defd4c84fcbdf1cada7ae0e
Tamil <tamil@bnsouth.com>
2020-03-24 10:24:43 -0400

    compilation erros fixed

--- recipes/src/main/java/com/oster/recipes/repositories/dynamodb/DataRepository.java
+++ recipes/src/main/java/com/oster/recipes/repositories/dynamodb/DataRepository.java
@@ -4,15 +4,14 @@ import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import com.oster.recipes.entities.dynamodb.Data;
-import com.oster.recipes.entities.dynamodb.Data3;
 import com.oster.recipes.entities.dynamodb.Data3.DataKey;
 
 @EnableScan
 public interface DataRepository extends CrudRepository<Data, DataKey>{
 		
-	Data3 findByPk(String pk);
+	Data findByPk(String pk);
 	
-	Data3 findByCategory(String category);
+	Data findByCategory(String category);
 	
-	Data3 findByCategoryContains(String category);
+	Data findByCategoryContains(String category);
 }
