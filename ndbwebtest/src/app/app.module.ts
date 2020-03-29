ada6340040062902e1ef128f87fa987bc6c95977
Tamil <tamil@bnsouth.com>
2020-03-29 12:46:38 -0400

    twitter integration

--- ndbwebtest/src/app/app.module.ts
+++ ndbwebtest/src/app/app.module.ts
@@ -7,7 +7,6 @@ import {CountryListComponent} from './country/country-list.component';
 import {CountryModule} from './country/country.module';
 import {CountryService} from './country/country.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
 import {AdminComponent} from './admin/admin.component';
 import {AdminService} from './admin/admin.service';
 import {AuthGuardService} from './auth/auth-guard.service';
@@ -28,7 +27,7 @@ import {PersonService} from './person/person.service';
 import {SharedModule} from './shared.module';
 import {StateService} from './state/state.service';
 import {CommonModule} from '@angular/common';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider, SocialLoginModule} from 'angularx-social-login';
 import {SocialAuthComponent} from './auth/social-auth/social-auth.component';
 import { SocialAuthService } from './auth/social-auth/social-auth.service';
@@ -59,7 +58,7 @@ export function provideConfig() {
     SocialAuthComponent
   ],
   imports: [
-    BrowserModule, ReactiveFormsModule, HttpModule, CountryModule, AppRoutingModule, FormsModule, SharedModule, SocialLoginModule
+    BrowserModule, ReactiveFormsModule, HttpClientModule, CountryModule, AppRoutingModule, FormsModule, SharedModule, SocialLoginModule
   ],
   providers: [
     {provide: AdminService, useClass: AdminService},
---
+++ recipes/src/main/java/com/oster/recipes/social/TwitterUtil.java
@@ -0,0 +1,58 @@
+package com.oster.recipes.social;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+@Component
+public class TwitterUtil {
+
+//	@Value("${twitter.api.key}")
+	private String apiKey;
+	
+//	@Value("${twitter.api.secret.key}")
+	private String apiSecretKey;
+	
+//	@Value("${twitter.access_token}")
+	private String accessToken;
+	
+//	@Value("${twitter.access_token.secret}")
+	private String accessTokenSecret;
+	
+	private Twitter client;
+	
+//	@PostConstruct
+	public void init() {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("DXIKf06dbix8doHHaKPDvCjal")
+		  .setOAuthConsumerSecret("pFyjwo9s6aQAcwU05q5qJS9LtF8rPBjPzHiGkaWmOBKa4TdM6I")
+//		  .setOAuthAccessToken("1244059013064658950-TdAqoA5IN4I31L4DL6QTYzIvJ5U6M3")
+//		  .setOAuthAccessTokenSecret("OUtTdeFlhVCEioYzFcRDT2lr8RqoA20uBqP78QcA9gcI2");
+		  .setOAuthAccessToken("1244059013064658950-TdAqoA5IN4I31L4DL6QTYzIvJ5U6M3")
+		  .setOAuthAccessTokenSecret("OUtTdeFlhVCEioYzFcRDT2lr8RqoA20uBqP78QcA9gcI2");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		client = tf.getInstance();
+	}
+	
+	public void tweet(String message) throws TwitterException {
+		
+		Status status = client.updateStatus("Welcome to tamils.rocks");
+		System.out.println(status.getText());
+	}
+	
+	public static void main(String[] args) {
+		try {
+			new TwitterUtil().tweet("test");
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+	}
+}
